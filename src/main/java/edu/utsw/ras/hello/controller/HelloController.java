1d113536785c53b6afb216f2e8632b5db63e359f
Chi-Fu Wang <chifuwang@yahoo.com>
2023-01-25 20:51:24 -0600

    Update HelloController.java

--- src/main/java/edu/utsw/ras/hello/controller/HelloController.java
+++ src/main/java/edu/utsw/ras/hello/controller/HelloController.java
@@ -1,4 +1,4 @@
-package controller;
+package edu.utsw.ras.hello.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public ResponseEntity<String> getHelloMessage() {
-        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    public String getHelloMessage() {
+        return "Hello";
     }
 }
